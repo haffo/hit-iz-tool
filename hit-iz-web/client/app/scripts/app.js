c1daae53d70910468658cf064ff1fd294bc8c3f7
Harold Affo <affoharold@yahoo.fr>
2018-10-12 17:59:51 -0400

    Fixing issues with transports

--- hit-iz-web/client/app/scripts/app.js
+++ hit-iz-web/client/app/scripts/app.js
@@ -766,7 +766,7 @@ app.run(function (Session, $rootScope, $location, $modal, TestingSettings, AppIn
 
 
   $rootScope.isDomainsManagementSupported = function () {
-    return $rootScope.getAppInfo().options && ($rootScope.getAppInfo().options['DOMAIN_MANAGEMENT_SUPPORTED'] === "true");
+    return $rootScope.getAppInfo().options && ($rootScope.getAppInfo().options['DOMAIN_MANAGEMENT_SUPPORTED'] === "true" || $rootScope.getAppInfo().options['DOMAIN_MANAGEMENT_SUPPORTED'] === true);
   };
 
     $rootScope.isLoggedIn = function () {
